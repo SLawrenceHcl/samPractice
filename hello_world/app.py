cd3114ba78a8d636bdc586e3c3db8666c418e1e6
Stephen <stephen.lawrence@hcl.com>
2022-09-28 15:57:27 -0700

    checking simpleTable

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,5 +1,9 @@
 import json
-
+from urllib import response
+import boto3
+import os
+import uuid
+from datetime import datetime
 
 def lambda_handler(event, context):
 
@@ -18,4 +22,41 @@ def lambda_handler2(event, context):
         "body": json.dumps({
             "personId": personId + " from Lambda"
         }),
+    }
+
+def createStudent(message, context):
+    if('body' not in message or
+        message['httpMethod'] != 'POST'):
+        return {
+            'statusCode': 400,
+            'headers': {},
+            'body': json.dumps({'msg': 'Bad Request'})
+        }
+    
+    table_name = os.environ.get('TABLE', "Student")
+    region = os.environ.get('REGION', 'us-west-1')
+
+    student_table = boto3.resource(
+        'dynamodb',
+        region_name=region
+    )
+
+    table = student_table.Table(table_name)
+    activity = json.loads(message['body'])
+
+    params = {
+        'id': str(uuid.uuid4()),
+        'studentName': activity['studentName']
+    }
+
+    response = table.put_item(
+        TableName=table_name,
+        Student=params
+    )
+    print(response)
+
+    return{
+        'statusCode': 201,
+        'headers': {},
+        'body': json.dumps({'msg': 'New Item Created'})
     }
